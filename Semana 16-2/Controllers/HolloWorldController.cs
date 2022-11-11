227997df6400c8bab683042e002bd00a5565661f
Alumnos <Alumnos@LABC2PC18>
2022-11-11 12:16:17 -0600

    Agregar archivos de proyecto.

---
+++ Semana 16-2/Controllers/HolloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace Semana_16_2.Controllers
+{
+    public class HolloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome()
+        {
+            return "Estas es la accion por defevto";
+        }
+
+        public string Parameters(string name, int edad)
+        {
+            return HtmlEncoder.Default.Encode($"HOLA{name}, tu edada es {edad} años");
+        }
+    }
+}
